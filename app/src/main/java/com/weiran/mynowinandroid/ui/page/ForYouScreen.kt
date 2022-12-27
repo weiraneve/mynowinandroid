db91577a6791a1305f5167bbda1c180735aa50f4
weiran1999 <907221539@qq.com>
2022-12-27 15:43:20 +0800

    feat: add done button and extract TopicItem and TopicSelection

--- app/src/main/java/com/weiran/mynowinandroid/ui/page/ForYouScreen.kt
+++ app/src/main/java/com/weiran/mynowinandroid/ui/page/ForYouScreen.kt
@@ -1,34 +1,46 @@
 package com.weiran.mynowinandroid.ui.page
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.weiran.mynowinandroid.R
+import com.weiran.mynowinandroid.data.source.LocalStorageImpl
+import com.weiran.mynowinandroid.ui.component.MyIcons
 import com.weiran.mynowinandroid.ui.component.MyTopBar
+import com.weiran.mynowinandroid.ui.component.TopicSelection
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ForYouScreen() {
-
+fun ForYouScreen(
+) {
     Scaffold(
         topBar = {
             MyTopBar(
                 modifier = Modifier.zIndex(-1F),
                 title = R.string.app_title,
+                actionIcon = MyIcons.Settings,
             )
         }
     ) { innerPadding ->
         Modifier.padding(innerPadding)
+
+        val topics = LocalStorageImpl(LocalContext.current).getTopics()
 
         Column(
             modifier = Modifier.padding(top = 80.dp)
@@ -50,7 +62,30 @@ fun ForYouScreen() {
                 style = MaterialTheme.typography.bodyMedium
             )
 
+            TopicSelection(
+                Modifier.padding(bottom = 8.dp),
+                topics
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .padding(horizontal = 40.dp)
+                        .width(364.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.onBackground
+                    ),
+                ) {
+                    Text(
+                        text = stringResource(R.string.done)
+                    )
+                }
+            }
+
         }
     }
-
 }
