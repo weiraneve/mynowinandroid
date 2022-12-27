3eaa09175f02a93348acce8d5b12436739d38f87
weiran1999 <907221539@qq.com>
2022-12-27 09:55:46 +0800

    feat: add blank demo page

---
+++ app/src/main/java/com/weiran/mynowinandroid/ui/home/HomePage.kt
@@ -0,0 +1,19 @@
+package com.weiran.mynowinandroid.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomePage() {
+    Text(text = "HomePage")
+}
+
+@Composable
+fun SavedPage() {
+    Text(text = "SavedPage")
+}
+
+@Composable
+fun InterestPage() {
+    Text(text = "InterestPage")
+}
