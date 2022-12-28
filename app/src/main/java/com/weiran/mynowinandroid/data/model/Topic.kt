107916d4f3832fd233d195fe26de7d991f62c7f4
weiran1999 <907221539@qq.com>
2022-12-28 15:28:06 +0800

    feat: refactor topic model

--- app/src/main/java/com/weiran/mynowinandroid/data/model/Topic.kt
+++ app/src/main/java/com/weiran/mynowinandroid/data/model/Topic.kt
@@ -3,9 +3,7 @@ package com.weiran.mynowinandroid.data.model
 data class Topic(
     val id: String,
     val name: String = "",
-    val shortDescription: String = "",
-    val longDescription: String = "",
     val url: String = "",
     val imageUrl: String = "",
-    val followed: Boolean = false,
+    val selected: Boolean = false,
 )
